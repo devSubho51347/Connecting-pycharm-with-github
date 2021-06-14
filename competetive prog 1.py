d2327b50cfc5e70f80154243b07a869c3f72a14a
devSubho51347 <choudhurysubhadeep51347@gmail.com>
2021-06-14 10:41:49 +0530

    The very second commit

---
+++ competetive prog 1.py
@@ -0,0 +1,16 @@
+# CODE FOR INSERTION SORT
+
+# li = [int(x) for x in input().split()]
+# for i in range(1,len(li),1):
+#     for j in range(i,0,-1):
+#         if li[j] > li[j-1]:
+#             break
+#         else:
+#             li[j], li[j-1] = li[j-1], li[j]
+# print(li)
+
+import numpy as np
+a = np.linspace(1,20,10,dtype=int,endpoint=False)
+print(a)
+print(type(a))
+print("How are you doing?")
